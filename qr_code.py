4e51e1dbfc22288783f38a80f8b38f5334a02d42
AYAN DHUA <74825068+AyanDhua@users.noreply.github.com>
2023-08-24 23:00:09 +0530

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,29 @@
+import qrcode
+from PIL import Image
+print("Let's make QR codes")
+while True:
+    inp = int(input("Please choose the desired option: \n 1: Make Simple QR \n 2: Generate customised QR Code \n Enter your choice: "))
+    match inp:
+        case 1:
+            url=input("Enter the text you want to make QR of: ")
+            name=input("Enter the name with which you want to save your QR: ")
+            img= qrcode.make(url)
+            img.save(f"{name}.png")
+            print("QR generated successfully ")
+            break
+        case 2:
+            url = input("Enter the text you want to make QR of: ")
+            name = input("Enter the name with which you want to save your QR: ")
+            b_size=int(input("Enter box size:"))
+            b=int(input("Enter border width:"))
+            q_color=input("Enter color for the QR code:")
+            q_bcolor= input("Enter background color for the QR code:")
+            qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=b_size,border=b)
+            qr.add_data(url)
+            qr.make(fit=True)
+            img=qr.make_image(fill_color=q_color,back_color=q_bcolor)
+            img.save(f"{name}.png")
+            print("QR generated successfully ")
+            break
+        case _ :
+            print("Please enter a valid Choice")
